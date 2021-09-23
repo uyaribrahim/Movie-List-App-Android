18b3ea6d9c0ec84221a03c0d0cedb78d877d0159
uyaribrahim <iuyar99@gmail.com>
2021-09-23 14:46:30 +0300

    add bottom navigation bar

--- app/src/main/java/com/uyaribrahim/movieapp/view/MainActivity.kt
+++ app/src/main/java/com/uyaribrahim/movieapp/view/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.uyaribrahim.movieapp.view
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.uyaribrahim.movieapp.view.MoviesFragmentDirections
 import com.uyaribrahim.movieapp.R
+import com.uyaribrahim.movieapp.databinding.ActivityMainBinding
+import com.uyaribrahim.movieapp.databinding.FragmentMoviesBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        setUpTabBar()
+    }
+
+    private fun setUpTabBar(){
+
+        binding.bottomNavBar.setItemSelected(R.id.nav_movies)
+        binding.bottomNavBar.setOnItemSelectedListener {
+            val action = MoviesFragmentDirections.actionMoviesFragmentToTvShowsFragment()
+            val secondAction = TvShowsFragmentDirections.actionTvShowsFragmentToMoviesFragment()
+            when (it){
+                R.id.nav_tvshows -> Navigation.findNavController(this,R.id.fragmentContainerView).navigate(action)
+                R.id.nav_movies -> Navigation.findNavController(this,R.id.fragmentContainerView).navigate(secondAction)
+            }
+        }
     }
 }
