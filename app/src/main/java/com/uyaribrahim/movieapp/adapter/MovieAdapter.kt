c17ed0b8e7185309b352a94489edb57de3917c20
uyaribrahim <iuyar99@gmail.com>
2021-09-28 00:56:49 +0300

    implement Glide and add internet permission

--- app/src/main/java/com/uyaribrahim/movieapp/adapter/MovieAdapter.kt
+++ app/src/main/java/com/uyaribrahim/movieapp/adapter/MovieAdapter.kt
@@ -1,9 +1,14 @@
 package com.uyaribrahim.movieapp.adapter
 
+import android.app.Activity
+import android.app.PendingIntent.getActivity
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.uyaribrahim.movieapp.R
 import com.uyaribrahim.movieapp.model.Movie
 import kotlinx.android.synthetic.main.item_movie_tvshow.view.*
@@ -23,6 +28,11 @@ class MovieAdapter(val movieList: ArrayList<Movie>): RecyclerView.Adapter<MovieA
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         holder.view.movieTitle.text = movieList[position].title
         holder.view.movieGenre.text = movieList[position].movieGenre
+
+        Glide.with(holder.view)
+            .load(movieList[position].moviePoster)
+            .placeholder(R.drawable.kateafismall)
+            .into(holder.view.movieImage)
     }
 
     override fun getItemCount(): Int {
